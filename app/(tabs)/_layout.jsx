698c61a48e7ff36de0dfe5fb44887ab74f189526
love olubummo <olubummolove3@gmail.com>
2024-07-02 18:47:56 +0100

    feature: updated tabBar color

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -7,10 +7,10 @@ const TabsLayout = () => {
   return (
     <>
       <Tabs>
-        <Tabs.Screen name='home' options={{title: "Home", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='home' color={color} size={24} />}}/>
-        <Tabs.Screen name='bookmark' options={{title:"Bookmark", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='bookmark' color={color} size={24}/>}}/>
-        <Tabs.Screen name='create' options={{title: "Create", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='add' color={color} size={24}/>}} />
-        <Tabs.Screen name='profile' options={{title: "Profile", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='person' color={color} size={24}/>}}/>
+        <Tabs.Screen name='home' options={{title: "Home", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='home' color={color} size={24} />, tabBarActiveTintColor: "#FFA001", tabBarInactiveTintColor: "#CDCDE0"}}/>
+        <Tabs.Screen name='bookmark' options={{title:"Bookmark", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='bookmark' color={color} size={24}/>, tabBarActiveTintColor: "#FFA001", tabBarInactiveTintColor: "#CDCDE0"}}/>
+        <Tabs.Screen name='create' options={{title: "Create", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='add' color={color} size={24}/>, tabBarActiveTintColor: "#FFA001", tabBarInactiveTintColor: "#CDCDE0"}} />
+        <Tabs.Screen name='profile' options={{title: "Profile", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='person' color={color} size={24}/>, tabBarActiveTintColor: "#FFA001", tabBarInactiveTintColor: "#CDCDE0"}}/>
       </Tabs> 
     </>
   )
