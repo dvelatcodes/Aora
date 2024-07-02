b776b34400b94ed596bcb4e1024e4b2948dd5575
love olubummo <olubummolove3@gmail.com>
2024-07-02 18:39:56 +0100

    created auth and tabs folder and files in them

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Tabs, Redirect } from 'expo-router';
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react'
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen name='home' options={{title: "Home", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='home' color={color} size={24} />}}/>
+        <Tabs.Screen name='bookmark' options={{title:"Bookmark", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='bookmark' color={color} size={24}/>}}/>
+        <Tabs.Screen name='create' options={{title: "Create", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='add' color={color} size={24}/>}} />
+        <Tabs.Screen name='profile' options={{title: "Profile", headerShown: false, tabBarIcon: ({color})=> <MaterialIcons name='person' color={color} size={24}/>}}/>
+      </Tabs> 
+    </>
+  )
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
--- app/index.jsx
+++ app/index.jsx
@@ -5,9 +5,9 @@ import { Link } from 'expo-router';
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Happy Phone</Text>
+      <Text className="text-3xl font-pblack">Happy Phone</Text>
       {/* <StatusBar style='auto'/> */}
-      <Link href="/profile" className='text-blue-400 mt-8'>Go to Profile</Link>
+      <Link href="/home" className='text-blue-400 mt-8'>Go to Home</Link>
     </View>
   )
 }
