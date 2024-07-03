b13e00af1e9cb6baba09163bec8fd07b6ca16ece
love olubummo <olubummolove3@gmail.com>
2024-07-03 02:52:51 +0100

    feature: integrated large text

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,34 @@
-import { ScrollView,Image, Text, View } from 'react-native'
-import { StatusBar } from 'expo-status-bar';
-import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { images } from '../constants';
+import { ScrollView, Image, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 const App = () => {
   return (
-    <SafeAreaView className='h-full bg-primary'>
-      <ScrollView contentContainerStyle={{height : '100%'}}>
-        <View className='w-full h-full justify-start items-center px-4'>
-            <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain'/>
-            <Image source={images.cards} className='max-w-[380px] w-full h-[300px]' resizeMode='contain'/>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full justify-start items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white text-center">
+              Discover Endless Possibilities With{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
