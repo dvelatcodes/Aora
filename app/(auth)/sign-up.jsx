c7f64bd727ed5b6087f70033db3974ffe978f4bb
love olubummo <olubummolove3@gmail.com>
2024-07-03 04:54:16 +0100

    feature: integrating sign-up page

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet,ScrollView, Text, View, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='h-full bg-primary'>
+      <ScrollView >
+          <View className='w-full h- justify-center px-4 my-7'>
+              <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]'/>
+              <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+                Sign Up
+              </Text>
+          </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
